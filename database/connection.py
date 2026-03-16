f10be89cb0d3d2e7712088d0d2f9fc78116a4582
bufonen <santigon8787@gmail.com>
2026-03-16 16:39:39 -0500

    feat: agregando pool para la conexión

--- database/connection.py
+++ database/connection.py
@@ -13,7 +13,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-    engine = create_engine(DATABASE_URL, echo=False)
+    engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 else:
     odbc_str = (
         f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes"
